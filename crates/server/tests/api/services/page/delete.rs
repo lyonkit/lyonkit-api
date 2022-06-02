d9596cae80dc12cd64eada63a2a1af1f5b1f7ef9
Léo Coletta <mail.leo.coletta@gmail.com>
2022-06-02 12:25:06 +0200

    test: Added delete page test

---
+++ crates/server/tests/api/services/page/delete.rs
@@ -0,0 +1,54 @@
+use crate::{helpers::TestApp, services::page::create::create_page};
+use reqwest::StatusCode;
+use serde_json::json;
+use test_context::test_context;
+
+#[test_context(TestApp)]
+#[tokio::test]
+async fn delete_page_works(ctx: &mut TestApp) {
+  ctx.create_api_key("namespace", false).await;
+
+  let page = create_page(
+    ctx,
+    &json!({
+        "path": "/path",
+        "title": "My Title",
+        "description": "My Description"
+    }),
+  )
+  .await;
+
+  let id = page
+    .get("id")
+    .and_then(|v| v.as_i64())
+    .expect("Expected ID");
+
+  let response = ctx.delete(format!("/page/{}", id)).await;
+  assert_eq!(StatusCode::OK, response.status());
+}
+
+#[test_context(TestApp)]
+#[tokio::test]
+async fn delete_page_from_other_namespace_should_be_denied(ctx: &mut TestApp) {
+  ctx.create_api_key("namespace", false).await;
+
+  let page = create_page(
+    ctx,
+    &json!({
+        "path": "/path",
+        "title": "My Title",
+        "description": "My Description"
+    }),
+  )
+  .await;
+
+  let id = page
+    .get("id")
+    .and_then(|v| v.as_i64())
+    .expect("Expected ID");
+
+  ctx.create_api_key("other_namespace", false).await;
+
+  let response = ctx.delete(format!("/page/{}", id)).await;
+  assert_eq!(StatusCode::NOT_FOUND, response.status());
+}
