6edae033f1aabd2baffd4acb1f58776171cc4951
Léo Coletta <mail.leo.coletta@gmail.com>
2022-06-16 15:22:08 +0200

    feat: Added typescript client

---
+++ clients/ts/src/index.ts
@@ -0,0 +1,132 @@
+import type { $Fetch } from 'ohmyfetch'
+import { $fetch } from 'ohmyfetch'
+import { joinURL } from 'ufo'
+
+export interface ImageInput {
+  alt?: string | null | undefined
+  image: Blob | File
+}
+
+export interface ImageOutput {
+  id: number
+  publicUrl: string
+  lazyImage: {
+    id: number
+    publicUrl: string
+    alt: string | null
+    createdAt: string
+    updatedAt: string
+  }
+  alt: string | null
+  createdAt: string
+  updatedAt: string
+}
+
+export interface PageInput {
+  title: string
+  description?: string | undefined | null
+  path: string
+}
+export interface PageOuput {
+  id: number
+  title: string
+  description: string | null
+  namespace: string
+  path: string
+  createdAt: string
+  updatedAt: string
+}
+export interface PageOuputWithBloks extends PageOuput {
+  bloks: BlokOuput[]
+}
+
+export interface BlokInput {
+  pageId: number
+  componentId: string
+  props: Record<string, any>
+  priority?: number | null | undefined
+}
+export interface BlokOuput {
+  id: number
+  pageId: number
+  componentId: string
+  props: Record<string, any>
+  priority: number
+  createdAt: string
+  updatedAt: string
+}
+
+export class LyonkitApiClient {
+  public readonly endpoint: string = 'https://lyonkit.leo-coletta.fr'
+  private readonly apiKey: string
+  private readonly fetch: $Fetch
+
+  constructor(params: { endpoint?: string; apiKey: string }) {
+    if (params.endpoint)
+      this.endpoint = params.endpoint
+
+    this.apiKey = params.apiKey
+    this.fetch = $fetch.create({
+      baseURL: joinURL(this.endpoint, '/api'),
+      headers: {
+        'x-api-key': this.apiKey,
+      },
+    })
+  }
+
+  // IMAGES
+
+  public async listImages(): Promise<ImageOutput[]> {
+    return this.fetch('/image')
+  }
+
+  public async createImage({ image, alt }: ImageInput): Promise<ImageOutput> {
+    const form = new FormData()
+    form.set('image', image)
+    return this.fetch('/image', { params: { alt }, method: 'POST', body: form })
+  }
+
+  public async deleteImage(imageId: number): Promise<ImageOutput> {
+    return this.fetch(`/image/${imageId}`, { method: 'DELETE' })
+  }
+
+  // PAGES
+
+  public async listPages(): Promise<PageOuput> {
+    return this.fetch('/page')
+  }
+
+  public async getPage(path: string): Promise<PageOuputWithBloks> {
+    return this.fetch('/page/wb', { params: { path } })
+  }
+
+  public async createPage(page: PageInput): Promise<PageOuput> {
+    return this.fetch('/page', { method: 'POST', body: page })
+  }
+
+  public async updatePage({ pageId, update }: { pageId: number; update: PageInput }): Promise<PageOuput> {
+    return this.fetch(`/page/${pageId}`, { method: 'PUT', body: update })
+  }
+
+  public async deletePage(pageId: number): Promise<PageOuput> {
+    return this.fetch(`/page/${pageId}`, { method: 'DELETE' })
+  }
+
+  // BLOKS
+
+  public async getBlok(blokId: number) {
+    return this.fetch(`/blok/${blokId}`)
+  }
+
+  public async createBlok(blok: BlokInput) {
+    return this.fetch('/blok', { method: 'POST', body: blok })
+  }
+
+  public async updateBlok({ blokId, update }: { blokId: number; update: BlokInput }) {
+    return this.fetch(`/blok/${blokId}`, { method: 'PUT', body: update })
+  }
+
+  public async deleteBlok(blokId: number) {
+    return this.fetch(`/blok/${blokId}`, { method: 'DELETE' })
+  }
+}
